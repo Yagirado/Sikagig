b8ccb8998bdc6ed6a54240ebb994be21a0f18c3f
yasmin128 <2410631170128@student.unsika.ac.id>
2026-09-13 20:43:58 +0700

    feat: add activity page

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
+import Activity from "./pages/activity/activity";
+import Test from "./test/test";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/activity" element={<Activity />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
 
